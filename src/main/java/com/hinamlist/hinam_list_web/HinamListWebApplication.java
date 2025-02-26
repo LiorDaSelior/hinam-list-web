26ff287c13de3ef611b47a6b43609dff558d3521
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2025-02-26 15:01:58 +0200

    init

---
+++ src/main/java/com/hinamlist/hinam_list_web/HinamListWebApplication.java
@@ -0,0 +1,13 @@
+package com.hinamlist.hinam_list_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinamListWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HinamListWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hinamlist/hinam_list_web/HinamListWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinamlist.hinam_list_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HinamListWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
